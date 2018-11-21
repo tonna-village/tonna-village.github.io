5951fa1f84cba0138228cc1937e432be5626263b
Simon Anthony <simontheengineer@gmail.com>
2018-11-21 15:27:34 +0000

    Add google analytics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+---
+---
+
 $("#nav-open").on("click", function() {
   $("body").addClass("no-scroll");
   $(".nav").fadeIn("fast");
@@ -7,6 +10,19 @@ $("#nav-close").on("click", function() {
   $(".nav").fadeOut("fast");
   $("body").removeClass("no-scroll");
 });
+
+{% if jekyll.environment == 'production' %}
+// Track navigation events with Google Analytics
+$(".nav a").on("click", function(e) {
+  const eventName = $(this).data("name");
+  ga("send", {
+    hitType: "event",
+    eventCategory: "Navigation",
+    eventAction: "selected",
+    eventLabel: eventName
+  });
+});
+{% endif %}
 
 // Disable touch highlight color: https://css-tricks.com/snippets/css/remove-gray-highlight-when-tapping-links-in-mobile-safari/
 document.addEventListener("touchstart", function() {}, true);
